80dae1a9acf2d4787c62c07f33b6adc28bf4c63a
hanneoberman <h.i.oberman@uu.nl>
2023-12-01 16:36:09 +0100

    First attempt

--- 2023/day/1/script.R
+++ 2023/day/1/script.R
@@ -1,8 +1,8 @@
 library(aochelpers)
-# input <- aoc_input_vector(1, 2022) # uncomment at end, once correct on test input
+# input <- aoc_input_vector(1, 2023) # uncomment at end, once correct on test input
 # also consider aoc_input_data_frame() or aoc_input_matrix(), with view = TRUE
 
-browseURL("https://adventofcode.com/2022/day/1")
+browseURL("https://adventofcode.com/2023/day/1")
 # use datapasta addin to copy in test input from the website
 # triple-click in test input box in the puzzle to select, 
 # then choose appropriate paste format from addin
